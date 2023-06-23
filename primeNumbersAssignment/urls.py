62feadf8c2869c7f31b0ac9883d75951988a5ad6
Piyush Alankar <piyushalankar251@gmail.com>
2023-06-23 23:48:48 +0530

    changed home route

--- primeNumbersAssignment/urls.py
+++ primeNumbersAssignment/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-
+from django.shortcuts import redirect
 from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('dishSearch.urls')),
+    path('', lambda request: redirect('/search')),
 ]
